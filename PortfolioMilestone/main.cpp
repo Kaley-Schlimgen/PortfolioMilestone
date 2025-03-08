267d15d87e1554fd042ec3144138ee7c6e5cf4fb
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-03-07 19:09:47 -0700

    Add try...catch

--- PortfolioMilestone/main.cpp
+++ PortfolioMilestone/main.cpp
@@ -16,33 +16,46 @@ std::condition_variable cv;
 bool reached_20 = false;
 
 void increment_counter() {
-    std::lock_guard<std::mutex> lock(counter_mutex);
-    std::cout << "First thread counting up to 20: " << std::endl;
-    for (int i = 1; i <= 20; ++i) {
-        counter = i;
-        std::cout << counter << std::endl;
+    try {
+        std::lock_guard<std::mutex> lock(counter_mutex);
+        std::cout << "First Thread Counting Up to 20: " << std::endl;
+        for (int i = 0; i <= 20; ++i) {
+            counter = i;
+            std::cout << counter << std::endl;
+        }
+        reached_20 = true;
+        cv.notify_one();
+    } catch (const std::exception& e) {
+        std::cerr <<"Error in First Thread: " << e.what() << std::endl;
     }
-    reached_20 = true;
-    cv.notify_one();
 }
 
 void decrement_counter() {
-    std::unique_lock<std::mutex> lock(counter_mutex);
-    cv.wait(lock, [] { return reached_20; });
-    
-    std::cout << "\nSecond thread counting down to 1: " << std::endl;
-    for (int i = 20; i > 0; --i) {
-        counter = i;
-        std::cout << counter << std::endl;
+    try {
+        std::unique_lock<std::mutex> lock(counter_mutex);
+        cv.wait(lock, [] { return reached_20; });
+        
+        std::cout << "\nSecond Thread Counting Down to 0: " << std::endl;
+        for (int i = 20; i >= 0; --i) {
+            counter = i;
+            std::cout << counter << std::endl;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error in Second Thread: " << e.what() << std::endl;
     }
 }
 
 int main() {
-    std::thread firstThread(increment_counter);
-    std::thread secondThread(decrement_counter);
-
-    firstThread.join();
-    secondThread.join();
+    try {
+        std::thread firstThread(increment_counter);
+        std::thread secondThread(decrement_counter);
+        
+        firstThread.join();
+        secondThread.join();
+    } catch (const std::exception& e) {
+        std::cerr << "Error in Main Class: " << e.what() << std::endl;
+        return 1;
+    }
     
     return 0;
 }
